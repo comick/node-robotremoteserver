2d771a707a892b914001493c07a703c700d47a57
Michele "comick" Comignano <comick@autistici.org>
2013-02-24 12:47:39 +0100

    initial

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 node-robotremoteserver
 ======================
 
-A node.js module providing the robot framework remote library interface 
+A node.js module providing the robot framework remote library interface.
+
+It's a heavy work in progress, still missing docs and tests.
+
+
+License
+=======
+
+Released under the MIT license. See the LICENSE file for the complete wording.
+
---
+++ example/examplelibrary.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var assert = require('assert')
+
+exports.count_items_in_directory = {
+	doc: 'Returns the number of items in the directory specified by `path`.',
+	args: ['path'],
+	impl: function(path) {
+		return fs.readdirSync(path).length
+	}
+}
+
+exports.strings_should_be_equal = {
+	doc: 'Returns the number of items in the directory specified by `path`.',
+	args: ['str1', 'str2'],
+	impl: function(str1, str2) {
+		console.log('Comparing \'%s\' to \'%s\'', str1, str2)
+		assert.equal(str1, str2, 'Given strings are not equal')
+	}
+}
+
+if (!module.parent) {
+	var robot = require('../lib/robotremoteserver')
+	var server = new robot.RobotRemoteServer([exports], 'localhost', 9090, true)
+}
+
